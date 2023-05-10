04968549fa123fb66737b2995666f5be4da30cb6
iftekharx <iftekharx86@gmail.com>
2023-05-10 09:48:44 +0600

    added image icon to jframe

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         frame.setTitle("JFrame title goes here");
         frame.setSize(420, 420);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        ImageIcon image = new ImageIcon("bread.png");
+        frame.setIconImage(image.getImage());
+
         frame.setVisible(true);
 
 
