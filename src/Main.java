c7e4c114ffb976cc1186d67d210484e30a401c9d
iftekharx <iftekharx86@gmail.com>
2023-05-10 09:42:56 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame title goes here");
+        frame.setSize(420, 420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+
+
+    }
+}
